8bd4da5a405b2e5650b620981dff5c0a56c1e6d3
译 <86699087+Giaoogle@users.noreply.github.com>
2024-06-14 09:18:01 +0800

    Update Wi-Fi_proxy.js

--- Wi-Fi_proxy.js
+++ Wi-Fi_proxy.js
@@ -11,7 +11,7 @@ Rewrite和Scripting依然有效
 */
 
 var wifiname = $network.wifi.ssid;
-var proxywifi = ["Giaomi"];
+var proxywifi = ["DAYUAN-5G"];
 for (var i = 0; i < proxywifi.length; i++) {
 	if (wifiname==proxywifi[i]){
 		$surge.setOutboundMode("direct");
